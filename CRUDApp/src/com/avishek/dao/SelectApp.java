2cee93ac795736365131c4dd43dfc38c5dce606c
Avishek Verma <avishekverma298@gmail.com>
2023-10-08 11:06:54 +0530

    PreparedStatement CRUD Operation completed

--- CRUDApp/src/com/avishek/dao/SelectApp.java
+++ CRUDApp/src/com/avishek/dao/SelectApp.java
@@ -11,11 +11,12 @@ public class SelectApp {
 	public static void main(String[] args) {
 		
 		Connection connection=null;
+		Statement statement=null;
+		ResultSet resultSet=null;
 		
 		try {
 //			1. Load and register the Driver
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		
 			System.out.println("Driver loaded Successfully...");
 			
 //			2. Establish the connection with dataBase
@@ -27,26 +28,23 @@ public class SelectApp {
 			System.out.println("Connection Object created...");
 			
 //			3. Create statement and execute the query
+			statement = connection.createStatement();
+			
 			String sqlSelectQuery = "select * from student";
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(sqlSelectQuery);
-			System.out.println("resulte got from DB...");
+			resultSet = statement.executeQuery(sqlSelectQuery);
 			
 //			4. Process the resultSet
 			System.out.println();
 			System.out.println("Id\tName\tAge\tAddress");
-			System.out.println();
 			while(resultSet.next()) {
 				Integer sid = resultSet.getInt(1);
 				String sname = resultSet.getString(2);
 				Integer sage = resultSet.getInt(3);
 				String saddress = resultSet.getString(4);
-				
 				System.out.println(sid+"\t"+sname+"\t"+sage+"\t"+saddress);
 			}
 			
 //			5. Handle the SQL exceptions
-			
 			
 //			6. Close the connection.
 			
@@ -56,8 +54,10 @@ public class SelectApp {
 			e.printStackTrace();
 		} finally {
 			try {
+				resultSet.close();
+				statement.close();
 				connection.close();
-				System.out.println("Connection closed.\nThanks for Using our Service.");
+				System.out.println("\nConnection closed.....\n---------Thanks for Using our Service.---------");
 				
 			} catch (SQLException e) {
 				e.printStackTrace();
